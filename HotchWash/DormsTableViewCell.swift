77729af9535e581094eb32fd0d11c3502a4cc2de
Daniel Kim <kdanielive@gmail.com>
2017-04-15 09:20:20 -0400

    First commit

---
+++ HotchWash/DormsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DormsTableViewCell.swift
+//  HotchWash
+//
+//  Created by Daniel Kim on 4/15/17.
+//  Copyright © 2017 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class DormsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dormLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
